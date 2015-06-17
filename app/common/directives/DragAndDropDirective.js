ae6dae8b940d7fcfa1f2c741c67a7a19b9bb3726
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2015-06-17 10:55:36 +0200

    fix

--- app/common/directives/DragAndDropDirective.js
+++ app/common/directives/DragAndDropDirective.js
@@ -2,7 +2,7 @@
 
 angular.module('StoryboardApp.Common')
 
-    .factory('$dragging', function () {
+    .factory("$dragging", [function () {
         var data = null;
         var type = null;
 
@@ -20,64 +20,86 @@ angular.module('StoryboardApp.Common')
             setType: function (newType) {
                 type = newType;
                 return type;
-            }
+            },
         };
-    })
+    }])
 
-    .directive('dragContainer', function () {
+    .directive("dragContainer", ["$parse", function ($parse) {
         return {
-            restrict: 'A',
-            controller: 'DragContainerController',
-            controllerAs: 'dragContainer',
+            restrict: "A",
+            require: "dragContainer",
+            controller: "DragContainerController",
+            controllerAs: "dragContainer",
             link: function ($scope, $element, $attrs, dragContainer) {
-                dragContainer.init($element);
+                dragContainer.init($element, $scope, {
+                    onDragStart: $parse($attrs.onDragStart),
+                    onDragEnd: $parse($attrs.onDragEnd),
+                });
 
-                $element.on('dragstart', dragContainer.handleDragStart.bind(dragContainer));
-                $element.on('dragend', dragContainer.handleDragEnd.bind(dragContainer));
+                $element.on("dragstart", dragContainer.handleDragStart.bind(dragContainer));
+                $element.on("dragend", dragContainer.handleDragEnd.bind(dragContainer));
+                // $element.on("dragenter", dragContainer.handleDragEnter.bind(dragContainer));
 
                 $scope.$watch($attrs.dragContainer, dragContainer.updateDragData.bind(dragContainer));
-                $attrs.$observe('mimeType', dragContainer.updateDragType.bind(dragContainer));
+                $attrs.$observe("mimeType", dragContainer.updateDragType.bind(dragContainer));
 
-                $attrs.$set('draggable', true);
+                $attrs.$set("draggable", true);
             }
         };
-    })
+    }])
 
-    .controller('DragContainerController', function ($dragging) {
+    .controller("DragContainerController", ["$dragging", function ($dragging) {
         var dragContainer = this;
 
-        dragContainer.init = function (el) {
+        dragContainer.init = function (el, scope, callbacks) {
             dragContainer.el = el;
+            dragContainer.scope = scope;
+            dragContainer.callbacks = callbacks;
         };
 
         dragContainer.handleDragStart = function (e) {
             if (e.originalEvent) e = e.originalEvent;
 
-            // console.log('handleDragStart', e);
-            e.dataTransfer.dropEffect = 'move';
-            e.dataTransfer.effectAllowed = 'move';
+            // console.log("handleDragStart", e);
 
-            dragContainer.el.addClass('drag-container-active');
+            try {
+                e.dataTransfer.setData(dragContainer.type, dragContainer.data);
+            } catch (ex) {
+                // Fallback for IE.. YAY!
+                e.dataTransfer.setData("text", dragContainer.data);
+            }
+            e.dataTransfer.effectAllowed = "move";
+            e.dataTransfer.dropEffect = "move";
+
+            dragContainer.el.addClass("drag-container-active");
             dragContainer.dragging = true;
 
             $dragging.setData(dragContainer.data);
             $dragging.setType(dragContainer.type);
+
+            if (dragContainer.callbacks.onDragStart) {
+                dragContainer.callbacks.onDragStart(dragContainer.scope, {$event: e});
+            }
         };
 
         dragContainer.handleDragEnd = function (e) {
             if (e.originalEvent) e = e.originalEvent;
 
-            angular.element(e.target).removeClass('drag-active');
+            angular.element(e.target).removeClass("drag-active");
 
-            dragContainer.el.removeClass('drag-container-active');
+            dragContainer.el.removeClass("drag-container-active");
             dragContainer.dragging = false;
 
             $dragging.setData(null);
             $dragging.setType(null);
+
+            if (dragContainer.callbacks.onDragEnd) {
+                dragContainer.callbacks.onDragEnd(dragContainer.scope, {$event: e});
+            }
         };
 
         dragContainer.updateDragData = function (data) {
-            // console.log('dragContainer.updateDragData', data);
+            // console.log("dragContainer.updateDragData", data);
 
             dragContainer.data = data;
 
@@ -85,20 +107,21 @@ angular.module('StoryboardApp.Common')
         };
 
         dragContainer.updateDragType = function (type) {
-            // console.log('dragContainer.updateDragType', type);
+            // console.log("dragContainer.updateDragType", type);
 
-            dragContainer.type = type || 'text/x-drag-and-drop';
+            dragContainer.type = type || "text/x-drag-data";
 
             if (dragContainer.dragging) $dragging.setType(dragContainer.type);
         };
-    })
+    }])
 
 
-    .directive('dropContainer', function ($document, $parse) {
+    .directive("dropContainer", ["$document", "$parse", function ($document, $parse) {
         return {
-            restrict: 'A',
-            controller: 'DropContainerController',
-            controllerAs: 'dropContainer',
+            restrict: "A",
+            require: "dropContainer",
+            controller: "DropContainerController",
+            controllerAs: "dropContainer",
             link: function ($scope, $element, $attrs, dropContainer) {
                 var bindTo = function (event) {
                     return function (e) {
@@ -119,42 +142,42 @@ angular.module('StoryboardApp.Common')
                     onDragEnter: $parse($attrs.onDragEnter),
                     onDragOver: $parse($attrs.onDragOver),
                     onDragLeave: $parse($attrs.onDragLeave),
-                    onDrop: $parse($attrs.onDrop)
+                    onDrop: $parse($attrs.onDrop),
                 });
 
-                $element.on('dragenter', handleDragEnter);
-                $element.on('dragover', handleDragOver);
-                $element.on('dragleave', handleDragLeave);
-                $element.on('drop', handleDrop);
+                $element.on("dragenter", handleDragEnter);
+                $element.on("dragover", handleDragOver);
+                $element.on("dragleave", handleDragLeave);
+                $element.on("drop", handleDrop);
 
                 $scope.$watch($attrs.accepts, dropContainer.updateMimeTypes.bind(dropContainer));
 
-                $document.on('dragend', dragEnd);
+                $document.on("dragend", dragEnd);
 
-                $scope.$on('$destroy', function () {
-                    $document.off('dragend', dragEnd);
+                $scope.$on("$destroy", function () {
+                    $document.off("dragend", dragEnd);
                 });
             }
         };
-    })
+    }])
 
-    .controller('DropContainerController', function ($dragging) {
+    .controller("DropContainerController", ["$dragging", function ($dragging) {
         var dropContainer = this;
         var targets = {};
-        var validAnchors = 'center top top-right right bottom-right bottom bottom-left left top-left'.split(' ');
+        var validAnchors = "center top top-right right bottom-right bottom bottom-left left top-left".split(" ");
 
         dropContainer.init = function (el, scope, callbacks) {
             dropContainer.el = el;
             dropContainer.scope = scope;
             dropContainer.callbacks = callbacks;
-            dropContainer.accepts = ['text/x-drag-and-drop'];
+            dropContainer.accepts = ["text/x-drag-data"];
 
-            dropContainer.el.addClass('drop-container');
+            dropContainer.el.addClass("drop-container");
         };
 
         dropContainer.addDropTarget = function (anchor, dropTarget) {
-            if (validAnchors.indexOf(anchor) < 0) throw new Error('Invalid anchor point ' + anchor);
-            if (targets[anchor]) throw new Error('Duplicate drop targets for the anchor ' + anchor);
+            if (validAnchors.indexOf(anchor) < 0) throw new Error("Invalid anchor point " + anchor);
+            if (targets[anchor]) throw new Error("Duplicate drop targets for the anchor " + anchor);
 
             targets[anchor] = dropTarget;
         };
@@ -168,10 +191,10 @@ angular.module('StoryboardApp.Common')
         };
 
         dropContainer.updateMimeTypes = function (mimeTypes) {
-            if (!mimeTypes) mimeTypes = ['text/x-drag-and-drop'];
+            if (!mimeTypes) mimeTypes = ["text/x-drag-data"];
             if (!angular.isArray(mimeTypes)) mimeTypes = [mimeTypes];
 
-            // console.log('dropContainer.updateMimeTypes', mimeTypes);
+            // console.log("dropContainer.updateMimeTypes", mimeTypes);
 
             dropContainer.accepts = mimeTypes;
         };
@@ -193,10 +216,10 @@ angular.module('StoryboardApp.Common')
                     var anchorX = width / 2;
                     var anchorY = height / 2;
 
-                    if (anchor.indexOf('left') >= 0) anchorX = 0;
-                    if (anchor.indexOf('top') >= 0) anchorY = 0;
-                    if (anchor.indexOf('right') >= 0) anchorX = width;
-                    if (anchor.indexOf('bottom') >= 0) anchorY = height;
+                    if (anchor.indexOf("left") >= 0) anchorX = 0;
+                    if (anchor.indexOf("top") >= 0) anchorY = 0;
+                    if (anchor.indexOf("right") >= 0) anchorX = width;
+                    if (anchor.indexOf("bottom") >= 0) anchorY = height;
 
                     var distanceSq = Math.pow(anchorX - e.offsetX, 2) + Math.pow(anchorY - e.offsetY, 2);
 
@@ -217,17 +240,17 @@ angular.module('StoryboardApp.Common')
                 anchor: activeAnchor,
                 target: activeTarget,
                 prevAnchor: prevAnchor,
-                prevTarget: prevTarget
+                prevTarget: prevTarget,
             };
 
             if (prevTarget !== activeTarget) {
                 if (prevTarget) {
-                    dropContainer.el.removeClass('drop-container-active-' + prevAnchor);
+                    dropContainer.el.removeClass("drop-container-active-" + prevAnchor);
                     prevTarget.handleDragLeave(eventData);
                 }
 
                 if (activeTarget) {
-                    dropContainer.el.addClass('drop-container-active-' + activeAnchor);
+                    dropContainer.el.addClass("drop-container-active-" + activeAnchor);
                     activeTarget.handleDragEnter(eventData);
                 }
             }
@@ -238,7 +261,7 @@ angular.module('StoryboardApp.Common')
         dropContainer.handleDragEnter = function (e) {
             if (e.originalEvent) e = e.originalEvent;
 
-            // console.log('handleDragEnter', e, dropContainer.accepts, $dragging.getType());
+            // console.log("handleDragEnter", e, dropContainer.accepts, $dragging.getType());
 
             if (!dropContainer.accepts || dropContainer.accepts.indexOf($dragging.getType()) >= 0) {
                 e.preventDefault();
@@ -249,7 +272,7 @@ angular.module('StoryboardApp.Common')
             var eventData = dropContainer.updateDragTarget(e);
 
             //dropContainer.el.children().css({'pointer-events': 'none'});
-            dropContainer.el.addClass('drop-container-active');
+            dropContainer.el.addClass("drop-container-active");
 
             if (dropContainer.callbacks.onDragEnter) {
                 dropContainer.callbacks.onDragEnter(dropContainer.scope, eventData);
@@ -259,7 +282,7 @@ angular.module('StoryboardApp.Common')
         dropContainer.handleDragOver = function (e) {
             if (e.originalEvent) e = e.originalEvent;
 
-            // console.log('dropContainer.handleDragOver', e);
+            // console.log("dropContainer.handleDragOver", e);
 
             if (!dropContainer.accepts || dropContainer.accepts.indexOf($dragging.getType()) >= 0) {
                 e.preventDefault();
@@ -281,10 +304,12 @@ angular.module('StoryboardApp.Common')
         dropContainer.handleDragLeave = function (e) {
             if (e.originalEvent) e = e.originalEvent;
 
+            // console.log("dropContainer.handleDragLeave", e);
+
             var eventData = dropContainer.updateDragTarget(e, true);
 
             dropContainer.el.children().css({'pointer-events': null});
-            dropContainer.el.removeClass('drop-container-active');
+            dropContainer.el.removeClass("drop-container-active");
 
             if (dropContainer.callbacks.onDragLeave) {
                 dropContainer.callbacks.onDragLeave(dropContainer.scope, eventData);
@@ -292,16 +317,20 @@ angular.module('StoryboardApp.Common')
         };
 
         dropContainer.handleDragEnd = function (e) {
-            // console.log('dropContainer.handleDragEnd', e);
+            if (e.originalEvent) e = e.originalEvent;
+
+            // console.log("dropContainer.handleDragEnd", e);
+
+            dropContainer.updateDragTarget(e, true);
 
             dropContainer.el.children().css({'pointer-events': null});
-            dropContainer.el.removeClass('drop-container-active');
+            dropContainer.el.removeClass("drop-container-active");
         };
 
         dropContainer.handleDrop = function (e) {
             if (e.originalEvent) e = e.originalEvent;
 
-            // console.log('dropContainer.handleDrop', e);
+            // console.log("dropContainer.handleDrop", e);
 
             if (!dropContainer.accepts || dropContainer.accepts.indexOf($dragging.getType()) >= 0) {
                 e.preventDefault();
@@ -319,23 +348,23 @@ angular.module('StoryboardApp.Common')
                 dropContainer.callbacks.onDrop(dropContainer.scope, eventData);
             }
 
-            dropContainer.handleDragEnd(e);
+            this.handleDragEnd(e);
         };
-    })
+    }])
 
-    .directive('dropTarget', function ($parse) {
+    .directive("dropTarget", ["$parse", function ($parse) {
         return {
-            restrict: 'A',
-            require: ['^dropContainer', 'dropTarget'],
-            controller: 'DropTargetController',
-            controllerAs: 'dropTarget',
+            restrict: "A",
+            require: ["^dropContainer", "dropTarget"],
+            controller: "DropTargetController",
+            controllerAs: "dropTarget",
             link: function ($scope, $element, $attrs, ctrls) {
                 var dropContainer = ctrls[0];
                 var dropTarget = ctrls[1];
-                var anchor = $attrs.dropTarget || 'center';
+                var anchor = $attrs.dropTarget || "center";
                 var destroy = dropContainer.removeDropTarget.bind(dropContainer, anchor);
 
-                $element.addClass('drop-target drop-target-' + anchor);
+                $element.addClass("drop-target drop-target-" + anchor);
 
                 dropTarget.init($element, $scope, {
                     onDragEnter: $parse($attrs.onDragEnter),
@@ -346,12 +375,13 @@ angular.module('StoryboardApp.Common')
 
                 dropContainer.addDropTarget(anchor, dropTarget);
 
-                $scope.$on('$destroy', destroy);
+                $scope.$on("$destroy", destroy);
             }
         };
-    })
+    }])
 
-    .controller('DropTargetController', function () {
+
+    .controller("DropTargetController", [function () {
         var dropTarget = this;
 
         dropTarget.init = function (el, scope, callbacks) {
@@ -361,9 +391,9 @@ angular.module('StoryboardApp.Common')
         };
 
         dropTarget.handleDragEnter = function (eventData) {
-            // console.log('dropTarget.handleDragEnter', eventData);
+            // console.log("dropTarget.handleDragEnter", eventData);
 
-            dropTarget.el.addClass('drop-target-active');
+            dropTarget.el.addClass("drop-target-active");
 
             if (dropTarget.callbacks.onDragEnter) {
                 dropTarget.callbacks.onDragEnter(dropTarget.scope, eventData);
@@ -371,7 +401,7 @@ angular.module('StoryboardApp.Common')
         };
 
         dropTarget.handleDragOver = function (eventData) {
-            // console.log('dropTarget.handleDragOver', eventData);
+            // console.log("dropTarget.handleDragOver", eventData);
 
             if (dropTarget.callbacks.onDragOver) {
                 dropTarget.callbacks.onDragOver(dropTarget.scope, eventData);
@@ -379,9 +409,9 @@ angular.module('StoryboardApp.Common')
         };
 
         dropTarget.handleDragLeave = function (eventData) {
-            // console.log('dropTarget.handleDragLeave', eventData);
+            // console.log("dropTarget.handleDragLeave", eventData);
 
-            dropTarget.el.removeClass('drop-target-active');
+            dropTarget.el.removeClass("drop-target-active");
 
             if (dropTarget.callbacks.onDragLeave) {
                 dropTarget.callbacks.onDragLeave(dropTarget.scope, eventData);
@@ -389,11 +419,13 @@ angular.module('StoryboardApp.Common')
         };
 
         dropTarget.handleDrop = function (eventData) {
-            // console.log('dropTarget.handleDrop', eventData);
+            // console.log("dropTarget.handleDrop", eventData);
 
             if (dropTarget.callbacks.onDrop) {
                 dropTarget.callbacks.onDrop(dropTarget.scope, eventData);
             }
         };
+    }])
 
-    });
+
+;
